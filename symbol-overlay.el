5bcd6d7e3f3b6501ccec3e6c378f33f7e7488c99
Steve Purcell <steve@sanityinc.com>
2021-01-18 21:07:02 +1300

    Revert "Merge pull request #71 from mpenet/feat/add-clojure-kw-list"
    This reverts commit 291dd270e3d3ce40139d44da3cb7b71e44fe9e9b, reversing
    changes made to 39f772b531117edba596e7a1210b3dbb87d56adb.
    
    See #72

--- symbol-overlay.el
+++ symbol-overlay.el
@@ -166,8 +166,7 @@
     (c++-mode . symbol-overlay-ignore-function-c++)
     (python-mode . symbol-overlay-ignore-function-python)
     (java-mode . symbol-overlay-ignore-function-java)
-    (go-mode . symbol-overlay-ignore-function-go)
-    (clojure-mode . symbol-overlay-ignore-function-clojure))
+    (go-mode . symbol-overlay-ignore-function-go))
   "Functions to determine whether a symbol should be ignored.
 
 This is an association list that maps a MAJOR-MODE symbol to a
@@ -537,14 +536,6 @@ BEG, END and LEN are the beginning, end and length of changed text."
      "char"     "final"     "interface"  "static"    "void"
      "class"    "finally"   "long"       "strictfp"  "volatile"
      "const*"   "float"     "native"     "super"     "while")))
-
-(defvar clojure-font-lock-extra-keywords)
-(defun symbol-overlay-ignore-function-clojure (symbol)
-  "Determine whether SYMBOL should be ignored (clojure)."
-  (symbol-overlay-match-keyword-list
-   symbol
-   (append clojure-font-lock-extra-keywords
-           '("defn" "def" "let" "deftest" "is"))))
 
 ;;; Commands
 
