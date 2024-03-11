a892fdbc478addf4c0662e08c56eaee92dd0e04b
roife <roifewu@gmail.com>
2024-03-11 15:37:05 +0800

    Fix incorrect behavior of symbol-overlay-switch-backward

--- symbol-overlay.el
+++ symbol-overlay.el
@@ -255,7 +255,7 @@ If SYMBOL is non-nil, get the overlays that belong to it.
 DIR is an integer.
 If EXCLUDE is non-nil, get all overlays excluding those belong to SYMBOL."
   (let ((overlays (cond ((= dir 0) (overlays-in (point-min) (point-max)))
-                        ((< dir 0) (overlays-in (point-min) (point)))
+                        ((< dir 0) (nreverse (overlays-in (point-min) (point))))
                         ((> dir 0) (overlays-in
                                     (if (looking-at-p "\\_>") (1- (point)) (point))
                                     (point-max))))))
